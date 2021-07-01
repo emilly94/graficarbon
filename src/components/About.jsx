8532f6fb12a31e1b821a91ef65eb74536d40fa85
Emilly <emillysporto.94@gmail.com>
2021-07-01 20:54:45 -0300

    Update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,26 +1,38 @@
 import React from "react";
-import { Image } from 'react-bootstrap';
-import Logo from "../style/img/logo.png";
-
-import "../style/css/style.css";
+import logo from '../style/img/logo.png'
+import {Image} from 'react-bootstrap';
+import { BsClipboardData, BsFillBriefcaseFill, BsEyeFill } from 'react-icons/bs';
 
 const About = ({id}) => {
-
   return (
     <section id={id}>
         <div id="mainSobreEmpresa" data-aos="fade-right">
-          <Image id="imgSobreEmpresa" src={Logo} />
-          <p>
-            A Graficarbon vem atuando no mercado de <span>carvão e grafite desde 2008,
-            dirigida por</span> profissionais experientes com mais de 20 anos <span>de
-            carreira no ramo.</span>
-            <br></br>
-            Somos uma empresa especializada, contando<span> com um quadro de
-            funcionários competentes</span><span>e capacitados na fabricação das peças.</span>
-            <br></br>
-            Garantimos total qualidade, agilidade e um <span>otimo atendimento na pré
-            e pós venda.</span>
-          </p>
+        <section class="py-5" id="features">
+                <div class="container px-5 my-5">
+                    <div class="row gx-5">
+                        <div class="col-lg-4 mb-5 mb-lg-0"><h2 class="fw-bolder mb-0"><Image src={logo} id="imgSobreEmpresa"/></h2></div>
+                        <div class="col-lg-8">
+                            <div class="row gx-5 row-cols-1 row-cols-md-2">
+                                <div class="col mb-5 h-100">
+                                    <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><BsFillBriefcaseFill/></div>
+                                    <h2 class="h5">Missão</h2>
+                                    <p class="mb-0">Entregar serviço de qualidade para a melhor satisfação do cliente</p>
+                                </div>
+                                <div class="col mb-5 h-100">
+                                    <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><BsEyeFill/></div>
+                                    <h2 class="h5">Visão</h2>
+                                    <p class="mb-0">PSer uma das maiores empresas no segmento de grafite do pais.</p>
+                                </div>
+                                <div class="col mb-5 mb-md-0 h-100">
+                                    <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><BsClipboardData/></div>
+                                    <h2 class="h5">Valores</h2>
+                                    <p class="mb-0">Ética, responsabilidade social, comprometimento e transparência.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
     </section>
   );
--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,43 +1,15 @@
 import React from "react";
-import { Carousel } from "react-bootstrap";
 import "../style/css/style.css";
-
-import Fabricacao from "../style/img/pessoasFabrica.jpg";
-import EntregaProdutos from "../style/img/entregaProdutoFabrica.jpg";
-
-import { FaBoxOpen, FaCity } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <section>
-      <Carousel>
-        <Carousel.Item>
-          <img
-            src={Fabricacao}
-            alt="First slide"
-            className="d-block w-90"
-            id="imgCarouselFabricacao"
-          />
-          <Carousel.Caption>
-            <h3><strong><FaCity/> Fabrica</strong></h3>
-            <p>Contamos com profissionais altamente <span>qualificado para realização da</span><span>peça de sua escolha.</span></p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            src={EntregaProdutos}
-            alt="Second slide"
-            className="d-block w-90"
-            id="imgCarousel"
-          />
-          <Carousel.Caption>
-            <h3><strong><FaBoxOpen/>Entrega</strong></h3>
-            <p>Nossa entrega é feita por transportadora<span>de sua preferencia o prazo varia de </span><span>acordo com a qualidade da peça.</span></p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-    </section>
-  );
-};
-
-export default Header;
+        <header class="masthead">
+                <div class="container">
+                    {/*<div class="masthead-subheading">Sintam-se</div>
+                    <div class="masthead-heading text-uppercase">Acolhidos</div>
+                    <a class="btn btn-primary btn-xl text-uppercase" href="#services">Nossos serviços</a>*/}
+                </div>
+            </header>
+      );
+    }
+    export default Header;
