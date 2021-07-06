c659049e8abd64d95469b14271e4092983ed019f
Emilly <emillysporto.94@gmail.com>
2021-07-06 20:34:14 -0300

    Update new template

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,6 @@ import { FiPhoneCall } from "react-icons/fi";
 import { BsBell } from "react-icons/bs";
 
 import "../style/css/style.css";
-import WhatsAppWidget from 'react-whatsapp-widget';
 import 'react-whatsapp-widget/dist/index.css';
 
 const Footer = ({id}) => {
--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "../style/css/style.css";
-import { Image } from "react-bootstrap";
-import { logo } from '../style/img/header-bg.jpeg'
+import { Link, animateScroll as scroll } from "react-scroll";
 const Header = () => {
   return (
     <header class="masthead">
@@ -9,26 +8,24 @@ const Header = () => {
         <div class="row gx-5 align-items-center">
           <div class="col-lg-6">
             <div class="mb-5 mb-lg-0 text-center text-lg-start">
-              <div class="display-1 lh-1 mb-3">Sejam</div>
-              <div class="display-1 lh-1 mb-3">Bem-vindos.</div>
-              <a class="btn btn-primary btn-lg" href="#products">Nossos produtos</a>
+            <h1 class="display-1 lh-1 mb-3">Sejam <br></br> Bem-vindos</h1>
+              <a class="btn btn-primary btn-lg" href="/products">
+                <Link
+                activeClass="active"
+                to="produtos"
+                spy={true}
+                smooth={true}
+                offset={-90}
+                duration={500}
+              >
+                Produtos
+              </Link></a>
             </div>
           </div>
           <div class="col-lg-6">
             <div class="masthead-device-mockup">
-              <svg class="circle" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-                <defs>
-                  <linearGradient id="circleGradient" gradientTransform="rotate(45)">
-                    <stop class="gradient-start-color" offset="0%"></stop>
-                    <stop class="gradient-end-color" offset="100%"></stop>
-                  </linearGradient>
-                </defs>
-                <circle cx="50" cy="50" r="50"></circle>
-              </svg>
               <div class="device-wrapper">
-                <div class="device">
-                </div>
-
+                <div class="device"></div>
               </div>
             </div>
           </div>
